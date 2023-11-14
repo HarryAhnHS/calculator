8112ab224f30a944a16ffc78e8257912a7b6c925
HarryAhnHS <hongsuhahn@gmail.com>
2023-11-14 21:16:46 +0800

    Completed backend for each operation call and operand set, handle edgecases in rounding and fitting display, evaluate function complete

--- main.js
+++ main.js
@@ -25,42 +25,66 @@ const plusminus = function(a) {
 }
 
 // Variables for operands and operator
-var firstNum = 0;
-var secondNum = 0;
-var operator = "";
+var operandLeft_ = 0;
+var operandRight_ = 0;
+var operator_ = "";
 
 // Operate function 
 const operate = function(a,b, operator) {
     if (operator == "+") {
-        add(a,b);
+        return add(a,b);
     }
     if (operator == "-") {
-        subtract(a,b);
+        return subtract(a,b);
     }
-    if (operator == "*") {
-        multiply(a,b);
+    if (operator == "x") {
+        return multiply(a,b);
     }
     if (operator == "/") {
-        divide(a,b);
-    }
-    if (operator == "^") {
-        power(a,b);
+        return divide(a,b);
     }
 }
-
 
 // Display Configuration
 const display = document.querySelector('.calc-display-number');
 
 // Append n to end of current display
 const populateDisplay = function(n) {
-    // Display function
-    display.textContent = (display.textContent.replace(/\s+/g, '')+n)
+    // Display n, but use scientific notation is length > 12, or round to fit display (12 chars) if decimal
+    if (n.toString().length>12) {
+        if (n > 999999999999) {
+            // Scientific notation
+            display.textContent = n.toPrecision(6);
+        }
+        else if (n < 1 && n >= 0) {
+            // Edge case - if n starts with 0.xxx then 0 is not significant digit, so 1 less to fit display
+            display.textContent = n.toPrecision(10);
+        } 
+        else {
+            display.textContent = n.toPrecision(11);
+        }
+    }
+    else {
+        display.textContent = n;
+    }
+};
+
+// Clear display {
+const clearDisplay = function() {
+    display.textContent = "";
 };
 
 // Convert display to float value for internal calculation
 const displayToFloat = function() {
     return parseFloat(display.textContent.replace(/\s+/g, ''));
+}
+
+// Reset Calculator
+const resetCalc = function() {
+    clearDisplay();
+    operandLeft_ = 0;
+    operandRight_ = 0;
+    operator_ = "";
 }
 
 // Event listeners for each number
@@ -70,20 +94,47 @@ const configs = document.querySelectorAll('.console.config');
 
 numbers.forEach( function(number) {
     number.addEventListener('click', e => {
+        // Clear if display is currently operator
+        if (display.textContent == '+' || display.textContent == '-' || display.textContent == 'x' || display.textContent == '/') {
+            clearDisplay();
+        }
+
         // Edgecase - only 12 char fit inside display panel (MONOSPACE FONT)
-        if (display.textContent.replace(/\s+/g, '').length <= 12) {
+        if (display.textContent.replace(/\s+/g, '').length < 12) {
             // Edgecase- only one . allowed in display
             if (number.textContent == '.') {
-                if (!display.textContent.includes('.')) {
+                if (!display.textContent.includes('.') && display.textContent.replace(/\s+/g, '').length > 0) {
                     display.textContent = display.textContent.replace(/\s+/g, '')+number.textContent;
                 }
-            } 
+            }
             else {
                 display.textContent = display.textContent.replace(/\s+/g, '')+number.textContent;
             }
-        }        
+        }
     });
 });
+
+operators.forEach( function(operator) {
+    operator.addEventListener('click', e => { 
+        if (operator.textContent == '=') {
+            if (operandLeft_ != 0 || operator != "") {
+                operandRight_ = displayToFloat();
+                clearDisplay();
+                populateDisplay(operate(operandLeft_,operandRight_,operator_));
+            }
+        }  
+        else {
+            if (display.textContent.length > 0) { // If display is not empty - set current to left operand global var
+                operator_ = operator.textContent; // set operator_ global variable for evaluation
+                operandLeft_ = displayToFloat();
+                clearDisplay();
+                populateDisplay(operator.textContent);
+            }
+        }
+    })
+})
+
+
 
 
 
